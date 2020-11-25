89f0b625c739fdedb6e761bf208e5a8e984bd02a
KingJzt <2876253980@qq.com>
2020-11-25 19:43:49 +0800

    add leetcode405

--- .vuepress/config/sidebar/index.js
+++ .vuepress/config/sidebar/index.js
@@ -29,7 +29,8 @@ module.exports={
                 'LeetCode/2020-11/72',
                 'LeetCode/2020-11/650',
                 'LeetCode/2020-11/204',
-                'LeetCode/2020-11/504'
+                'LeetCode/2020-11/504',
+                'LeetCode/2020-11/405'
             ]
         },{
             title: 'Git',
